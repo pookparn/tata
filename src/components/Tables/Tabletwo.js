004df215e3b6fedf6a99d7ab077fc69af2a953b4
Peeti Kaewsa-ard <pookparn@gmail.com>
2019-08-06 23:16:54 +0700

    update

--- src/components/Tables/Tabletwo.js
+++ src/components/Tables/Tabletwo.js
@@ -9,6 +9,7 @@ import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 import Select from '@material-ui/core/Select';
 import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
 
 const useStyles = makeStyles(theme => ({
     root: {
@@ -21,16 +22,16 @@ const useStyles = makeStyles(theme => ({
     },
 }));
 
-function createData(name, calories, fat, carbs, protein) {
-    return { name, calories, fat, carbs, protein };
+function createData(name, calories, fat, carbs, protein,age) {
+    return { name, calories, fat, carbs, protein ,age};
 }
 
 const rows = [
-    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-    createData('Eclair', 262, 16.0, 24, 6.0),
-    createData('Cupcake', 305, 3.7, 67, 4.3),
-    createData('Gingerbread', 356, 16.0, 49, 3.9),
+    createData('Frozen yoghurt', 159, 6.0, 24, 4.0,10),
+    createData('Ice cream sandwich', 237, 9.0, 37, 4.3,20),
+    createData('Eclair', 262, 16.0, 24, 6.0,30),
+    createData('Cupcake', 305, 3.7, 67, 4.3,10),
+    createData('Gingerbread', 356, 16.0, 49, 3.9,20),
 ];
 
 export default function SimpleTable() {
@@ -41,25 +42,12 @@ export default function SimpleTable() {
             <Table className={classes.table}>
                 <TableHead>
                     <TableRow>
-                        <TableCell>Dessert (100g serving)
-            <InputLabel htmlFor="age-simple">Age</InputLabel>
-                            <Select
-                                value="Thirty"
-                                
-                                inputProps={{
-                                    name: 'age',
-                                    id: 'age-simple',
-                                }}
-                            >
-                                <MenuItem value={10}>Ten</MenuItem>
-                                <MenuItem value={20}>Twenty</MenuItem>
-                                <MenuItem value={30}>Thirty</MenuItem>
-                            </Select>
-                        </TableCell>
+                        <TableCell>Dessert (100g serving)</TableCell>
                         <TableCell align="right">Calories</TableCell>
                         <TableCell align="right">Fat&nbsp;(g)</TableCell>
                         <TableCell align="right">Carbs&nbsp;(g)</TableCell>
                         <TableCell align="right">Protein&nbsp;(g)</TableCell>
+                        <TableCell align="right">Last</TableCell>
                     </TableRow>
                 </TableHead>
                 <TableBody>
@@ -72,6 +60,19 @@ export default function SimpleTable() {
                             <TableCell align="right">{row.fat}</TableCell>
                             <TableCell align="right">{row.carbs}</TableCell>
                             <TableCell align="right">{row.protein}</TableCell>
+                            <TableCell align="right">            
+                            <InputLabel htmlFor="age-simple">Age</InputLabel>
+                            <Select
+                                value={row.age}
+                                inputProps={{
+                                    name: 'age',
+                                    id: 'age-simple',
+                                }}
+                            >
+                                <MenuItem value={10}>Ten</MenuItem>
+                                <MenuItem value={20}>Twenty</MenuItem>
+                                <MenuItem value={30}>Thirty</MenuItem>
+                            </Select></TableCell>
                         </TableRow>
                     ))}
                 </TableBody>
